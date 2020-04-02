57985c7453a253b20a25dda0162d419d0e7196d2
Trevor Trusty <trevortrusty@outlook.com>
2020-04-02 17:23:02 -0400

    Rickroll update

---
+++ script.js
@@ -0,0 +1,6 @@
+var rickroll = document.getElementById("rickroll");
+
+function theme()
+{
+	rickroll.innerHTML = "<meta http-equiv=\"refresh\" content=\"0; URL=\'https://www.youtube.com/watch?v=dQw4w9WgXcQ'\" />";
+}
